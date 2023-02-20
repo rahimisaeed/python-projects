25199329bd75f89d60d83efa2ae392eb565dfa45
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-02-20 23:44:24 +0330

    add conda env to gitignore

--- python-tutorial-examples/example.py
+++ python-tutorial-examples/example.py
@@ -1,10 +1,24 @@
-import numpy as np
-import matplotlib as plt
+# import numpy as np
+# import matplotlib as plt
 
-a = np.array([[1, 2], [3, 5]])
-print(a)
-b = 1
-c = 10
-print("done")
-r = 1
-e = 1
+def over_nine_thousand(lst):
+  sum_element = 0
+  new_lst = []
+  index = 0
+  while sum_element < 9000:
+    # for index in range(len(lst)):
+      sum_element += lst[index]
+      # print(sum_element)
+      # new_lst.append(lst[index])
+      # print(new_lst)
+      index +=1
+  return sum_element
+  # sum = 0
+  # for index in lst:
+  #   sum +=index
+  #   if sum >9000:
+  #     break
+  # return sum
+
+#Uncomment the line below when your function is done
+print(over_nine_thousand([8000, 900, 120, 5000]))
