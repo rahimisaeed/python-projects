4dfccc4bf8495d128b4eed69cde3dcf4cd16755e
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-03-02 23:03:24 +0330

    Update codecademy_project_Basta_Fazoolin.py

--- python-tutorial-examples/codecademy_project_Basta_Fazoolin.py
+++ python-tutorial-examples/codecademy_project_Basta_Fazoolin.py
@@ -1,26 +1,47 @@
 
 brunch_items = {
-  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00,
-  'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00,
-  'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+    'pancakes': 7.50,
+    'waffles': 9.00,
+    'burger': 11.00,
+    'home fries': 4.50,
+    'coffee': 1.50,
+    'espresso': 3.00,
+    'tea': 1.00,
+    'mimosa': 10.50,
+    'orange juice': 3.50
 }
 
 early_bird_items = {
-  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00,
-  'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00,
-  'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+    'salumeria plate': 8.00,
+    'salad and breadsticks (serves 2, no refills)': 14.00,
+    'pizza with quattro formaggi': 9.00,
+    'duck ragu': 17.50,
+    'mushroom ravioli (vegan)': 13.50,
+    'coffee': 1.50, 'espresso': 3.00,
 }
 
+
 dinner_items = {
-  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00,
-  'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00,
-  'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+    'crostini with eggplant caponata': 13.00,
+    'caesar salad': 16.00,
+    'pizza with quattro formaggi': 11.00,
+    'duck ragu': 19.50,
+    'mushroom ravioli (vegan)': 13.50,
+    'coffee': 2.00,
+    'espresso': 3.00,
 }
 
 kids_items = {
-  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00,
-  'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00,
-  'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+    'chicken nuggets': 6.50,
+    'fusilli with wild mushrooms': 12.00,
+    'apple juice': 3.00
+}
+
+arepa_items = {
+    'arepa pabellon': 7.00,
+    'pernil arepa': 8.50,
+    'guayanes arepa': 8.00,
+    'jamon arepa': 7.50
 }
 
 
@@ -44,23 +65,58 @@ class Menu:
         return total_bill
 
 
+brunch = Menu('Brunch', brunch_items, 1100, 1600)
+early_bird = Menu('Early Bird', early_bird_items, 1500, 1800)
+dinner = Menu('Dinner', dinner_items, 1700, 2300)
+kids = Menu('Kids', kids_items, 1100, 2100)
+arepa = Menu('Take a Arepa', arepa_items, 1000, 2000)
+
+
 class Franchise:
 
     def __init__(self, address, menus):
         self.address = address
         self.menus = menus
-    
-    def __repre__(self):
-        return "The address of the restaurant is {add}".format(add=self.address)
+
+    def __repr__(self):
+        return "The address of the restaurant is {add}.".format(add=self.address)
+
+    def available_menus(self, time):
+        available_menus = []
+        for menu in self.menus:
+            if time >= menu.start_time and time <= menu.end_time:
+                available_menus.append(menu)
+        return available_menus
 
 
-flagship_store = Franchise("1232 West End Road", [brunch_items, early_bird_items, dinner_items, kids_items])
-new_installement = Franchise("12 East Mulberry Street", [brunch_items, early_bird_items, dinner_items, kids_items])
+flagship_store = Franchise('1232 West End Road', [
+                           brunch, early_bird, dinner, kids])
+new_installement = Franchise('12 East Mulberry Street', [
+                             brunch, early_bird, dinner, kids])
+arepa_franchise = Franchise('189 Fitzgerald Avenue', [arepa])
 
-brunch = Menu("brunch", brunch_items, 11, 16)
 
+class Bussiness:
+    def __init__(self, name, franchises):
+        self.name = name
+        self.franchises = franchises
+
+
+basta_bussiness = Bussiness("Basta Fazoolin' with my heart", [
+    flagship_store, new_installement])
+arepa_bussiness = Bussiness('Take a Arepa', [arepa_franchise])
+
+
+# tests
 brunch_bill = brunch.calculate_bill(['pancakes', 'home fries', 'coffee'])
+kids_bill = kids.calculate_bill(
+    ['pancakes', 'fusilli with wild mushrooms', 'coffee'])
 
 print(brunch_bill)
+print(kids_bill)
 print(brunch)
+print(kids)
 print(flagship_store)
+
+print(flagship_store.available_menus(1800))
+print(arepa_bussiness.franchises[0].menus[0])
