689065c7d1ddf1e007dfce70b1f47593f04242f7
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-03-08 23:45:02 +0330

    hanoi project

--- python-tutorial-examples/data-structure-and-algorithm/towers_of_hanoi.py
+++ python-tutorial-examples/data-structure-and-algorithm/towers_of_hanoi.py
@@ -3,12 +3,62 @@ from stack_class import Stack
 print("\nLet's play Towers of Hanoi!!")
 
 # Create the Stacks
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
 
+stacks = []
+stacks += [left_stack, middle_stack, right_stack]
 
 # Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while num_disks < 3:
+    num_disks = int(input("\nEnter a number greater than or equal to 3:\n"))
 
+for disk in range(num_disks, 0, -1):
+    left_stack.push(disk)
+
+num_optimal_moves = 2**(num_disks) - 1
+print("\nThe fastest you can solve this game is in {moves} moves".format(
+    moves=num_optimal_moves))
 
 # Get User Input
 
 
+def get_input():
+    # choices = ['L', 'M', 'R']
+    choices = [stack.get_name()[0] for stack in stacks]
+
+    while True:
+        for i in range(len(stacks)):
+            name = stacks[i].get_name()
+            letter = choices[i]
+            print("Enter {letter} for {name}".format(letter=letter, name=name))
+
+        user_input = input("")
+
+        if user_input in choices:
+            for i in range(len(stacks)):
+                if user_input == choices[i]:
+                    return stacks[i]
+
+
 # Play the Game
+num_user_moves = 0
+
+while (right_stack.get_size() != num_disks):
+    print("\n\n\n...Current Stacks...")
+    for stack in stacks:
+        stack.print_items()
+
+    while True:
+        print("\nWhich stack do you want to move from?\n")
+        from_stack = get_input("")
+        print("which stack do you want to move to?\n")
+        to_stack = get_input("")
+
+        if from_stack is None:
+            print("\n\nInvalid Move. Try Again.")
+
+        elif to_stack is None or stack
+            
